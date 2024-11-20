6e884c876eca5cca8704c4ecfa9738d3385cf3c2
King-A-19 <116169604+King-A-19@users.noreply.github.com>
2024-11-20 18:34:38 +0000

    Updated code with latest changes

---
+++ CRASH_COURSE/file.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+// fs.readFile('./docs/blogs.txt', (err, data) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
+
+// console.log('last line');
+
+// fs.writeFile('./docs/blogs.txt', 'hello, world', () => {
+//     console.log('it was written')
+// });
+
+// fs.writeFile('./docs/blogs1.txt', 'hello, again', () => {
+//     console.log('it was written')
+// });
+if(!fs.existsSync('./assets')){
+    fs.mkdir('./assets', (err) => {
+        if(err){
+            console.log(err);
+        }
+        console.log('folder created');
+
+});
+}else{
+    fs.rmdir('./assets', (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('folder deleted');
+    })
+}
---
+++ CRASH_COURSE/test.js
@@ -0,0 +1,10 @@
+// const name = 'king';
+
+// console.log(name);
+
+const greet = (name) => {
+    console.log(`hello, ${name}`);
+}
+
+greet('mario');
+greet('yoshi');
